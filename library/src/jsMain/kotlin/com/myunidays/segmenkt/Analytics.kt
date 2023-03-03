36ebbfd872e7f22484375e14d706139f026cc02b
OliverMcBurney <oliver.mcburney@myunidays.com>
2023-03-03 12:28:46 +0000

    [opt-out] fix

--- library/src/jsMain/kotlin/com/myunidays/segmenkt/Analytics.kt
+++ library/src/jsMain/kotlin/com/myunidays/segmenkt/Analytics.kt
@@ -28,6 +28,8 @@ actual class Analytics {
     actual fun group(groupId: String, traits: Map<Any?, *>?, options: Map<Any?, *>?) {
     }
 
+    actual fun optOut(status: Boolean) {
+    }
     actual fun reset() {
     }
 }
