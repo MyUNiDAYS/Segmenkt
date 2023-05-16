fa08e10971c5d1fc3f6d65f5cbe4c2aa69142cd3
Andrew Reed <andrew_reed@hotmail.com>
2023-05-16 14:05:09 +0100

    Added ios support for firebase integration

--- library/src/iosMain/kotlin/com/myunidays/segmenkt/FirebaseIntegration.kt
+++ library/src/iosMain/kotlin/com/myunidays/segmenkt/FirebaseIntegration.kt
@@ -9,29 +9,16 @@ import com.myunidays.segmenkt.integrations.ScreenPayload
 import com.myunidays.segmenkt.integrations.TrackPayload
 
 actual class FirebaseIntegration internal constructor(
-    private val ios: cocoapods.SegmentFirebase.SEGFirebaseIntegration
+    private val ios: cocoapods.Analytics.SEGIntegrationProtocol
 ) : Integration<FirebaseIntegration> {
-    override fun identify(identifyPayload: IdentifyPayload) =
-        ios.identify(identifyPayload as cocoapods.SegmentFirebase.SEGIdentifyPayload)
-
-    override fun group(groupPayload: GroupPayload) =
-        ios.group(groupPayload as cocoapods.SegmentFirebase.SEGGroupPayload)
-
-    override fun track(trackPayload: TrackPayload) =
-        ios.track(trackPayload as cocoapods.SegmentFirebase.SEGTrackPayload)
-
-    override fun alias(aliasPayload: AliasPayload) =
-        ios.alias(aliasPayload as cocoapods.SegmentFirebase.SEGAliasPayload)
-
-    override fun screen(screenPayload: ScreenPayload) =
-        ios.screen(screenPayload as cocoapods.SegmentFirebase.SEGScreenPayload)
-
+    override fun identify(identifyPayload: IdentifyPayload) = ios.identify(identifyPayload)
+    override fun group(groupPayload: GroupPayload) = ios.group(groupPayload)
+    override fun track(trackPayload: TrackPayload) = ios.track(trackPayload)
+    override fun alias(aliasPayload: AliasPayload) = ios.alias(aliasPayload)
+    override fun screen(screenPayload: ScreenPayload) = ios.screen(screenPayload)
     override fun flush() = ios.flush()
-
     override fun reset() = ios.reset()
-
-    override fun debug(debug: Boolean) {
-    }
+    override fun debug(debug: Boolean) { }
 
     actual companion object {
         actual fun factory(
